7c708c2372a392b4ef8f45020b2e4794494caf97
MistressSaeko <asukablaze@gmail.com>
2020-10-31 06:32:54 -0400

    add texture loading

---
+++ Clothier3D/Texture.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using OpenTK.Graphics.OpenGL4;
+using PixelFormat = OpenTK.Graphics.OpenGL4.PixelFormat;
+
+namespace Clothier3D
+{
+    class Texture
+    {
+        public readonly int Handle;
+
+        public Texture(string Path)
+        {
+            Handle = GL.GenTexture();
+
+            Use();
+
+            using (var image = new Bitmap(Path))
+            {
+                var Data = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height, 0, PixelFormat.Bgra, PixelType.UnsignedByte, Data.Scan0);
+            }
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+        }
+
+        public void Use(TextureUnit Unit = TextureUnit.Texture0)
+        {
+            GL.ActiveTexture(Unit);
+            GL.BindTexture(TextureTarget.Texture2D, Handle);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Create 3D clothes for 3D figures using patterns.
 Requires Visual Studio 2019 Community Edition.  
 
 Clothier3D is Copyright (C) 2020 Silky Softworks 
+
+BulletSharp/OpenTK - MIT License  
